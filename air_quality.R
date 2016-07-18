8456031bc97218dbb2ce15f0c22a330a8260e904
nick3499 <indiessance@gmail.com>
2016-07-18 13:33:07 -0500

    Added Link
    https://archive.ics.uci.edu/ml/datasets/Air+Quality

--- air_quality.R
+++ air_quality.R
@@ -1,9 +1,6 @@
 #!/usr/bin/r
 
+# https://archive.ics.uci.edu/ml/datasets/Air+Quality
 air_quality <- read.csv("air_quality_uci.csv", header = TRUE, sep = ";")
-
 # plot for readings over span of first 24 hours
 plot(air_quality[2:27,3:4], main="CO Concentration")
-
-
-# majority of CO emissions to ambient air come from mobile sources - EPA
